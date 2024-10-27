4d1af28f80c03224c7ea08cfab0b0ebe37ebe9f1
balu <balamamidi2002@gmail.com>
2024-10-27 22:56:28 +0530

    day1

---
+++ src/Components/Nav.js
@@ -0,0 +1,17 @@
+function Nav(){
+    return
+    (
+        <div className="Nav">
+            <ul>
+                <li><i class="bi bi-linkedin"></i></li>
+                <li>Home</li>
+                <li>About</li>
+            </ul>
+            <ul>
+
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
